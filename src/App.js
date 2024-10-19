4205c1cb449b82fd9115a310aee76476a7d68b5a
aaldelaimy <awaldelaimy@gmail.com>
2024-10-19 16:51:16 -0700

    Site Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import screenshot from './images/ayoob.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div>
+      <h1>Welcome to my webpage</h1> 
+      <h2>About Me</h2>
+      <b>this is bold </b><br></br>
+      <i>woahhh</i>
+      <ul>
+        <li><bi>Coding</bi></li>
+        <li><bi>Music</bi></li>
+        <li><bi>Travelling</bi></li>
+      </ul>
+      <p>Welcome to my first React app.</p>
+      <h2>My Skills</h2>
+      <ol>
+      <li>HTML & CSS</li>
+    <li>JavaScript</li>
+    <li>Python</li>
+      </ol>
+      <h3>My Favorite Websites</h3>
+      <a href="https://www.youtube.com/watch?v=-3AmiKlbFLs">Visit Example Website</a>
+      <div className="App">
+        <h1>
+          <img
+            src={screenshot}
+            alt="ayoob"
+            className="image"
+            />
+        </h1>
+      </div>
     </div>
   );
 }
-
 export default App;
